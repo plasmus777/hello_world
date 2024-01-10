ab57a80ac701a05e6c32256ff85b4f3ba41a146b
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-01-10 16:57:15 -0300

    Update HelloWorldController.java

--- helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -8,9 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
-	@GetMapping
+	@GetMapping("/hello")
 	public String helloWorld() {
-		return "Hello World!!!";
+		return "Hello World!";
 	}
-	
+
+	@GetMapping("/bsms")
+	public String[] mostraBSMs() {
+		return new String[]{"Persistência",
+			"Mentalidade de Crescimento",
+			"Responsabilidade Pessoal",
+			"Orientação ao Futuro",
+			"Orientação ao Detalhe",
+			"Trabalho em Equipe",
+			"Comunicação",
+			"Proatividade"};
+	}
+
+	@GetMapping("/objetivos")
+	public String[] listaObjetivos() {
+		return new String[] {"Entender como trabalhar com banco de dados",
+				"Desenvolver parte do projeto integrador",
+				"Aprender a trabalhar com SpringBoot",
+				"Conseguir manter as tarefas em dia"};
+	}
 }
